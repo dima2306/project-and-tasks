3ca1f3cdf7d000eae80ec7e5e2e651da7bb8af25
dima23 <dima.simonishvili@gmail.com>
2025-09-30 00:46:20 +0400

    Adds tasks relationship to Project model
    Signed-off-by: dima23 <dima.simonishvili@gmail.com>

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -12,6 +12,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
@@ -27,6 +28,11 @@ class Project extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
     }
 
     protected function casts(): array
