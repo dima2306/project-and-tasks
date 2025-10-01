26921a6d49916ca565e4b84aacbf9e71e26ea344
dima23 <dima.simonishvili@gmail.com>
2025-10-01 12:54:11 +0400

    Adds role-based permissions and user seeding functionality
    Signed-off-by: dima23 <dima.simonishvili@gmail.com>

---
+++ database/seeders/PermissionsAndRolesSeeder.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dima23
+ * Date: 01.10.25
+ * Time: 12:31
+ */
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissionsAndRolesSeeder extends Seeder
+{
+    public function run(): void
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Permission::create(['name' => 'create projects']);
+        Permission::create(['name' => 'edit projects']);
+        Permission::create(['name' => 'delete projects']);
+        Permission::create(['name' => 'view projects']);
+        Permission::create(['name' => 'create tasks']);
+        Permission::create(['name' => 'edit tasks']);
+        Permission::create(['name' => 'delete tasks']);
+        Permission::create(['name' => 'view tasks']);
+
+        $adminRole = Role::create(['name' => 'admin']);
+
+        /**
+         * For simplicity, we assign all permissions here.
+         * Preferably we do this in AppServiceProvider
+         * via Gate::before to give all permissions.
+         */
+        $adminRole->givePermissionTo(Permission::all());
+
+        $userRole = Role::create(['name' => 'user']);
+        $userRole->givePermissionTo(['view projects', 'view tasks']);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dima23
+ * Date: 01.10.25
+ * Time: 12:39
+ */
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $user = User::firstOrCreate(
+            ['email' => 'user@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+            ]
+        );
+        $user->assignRole('user');
+
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Admin User',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+            ]
+        );
+        $admin->assignRole('admin');
+    }
+}
