30fd6713f2dbfa65fee07eb96a3d9eb25aa9e094
dima23 <dima.simonishvili@gmail.com>
2025-10-02 15:09:20 +0400

    Adds hasManyThrough relationship for tasks in User model
    Signed-off-by: dima23 <dima.simonishvili@gmail.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -58,5 +59,10 @@ class User extends Authenticatable
     public function projects(): HasMany
     {
         return $this->hasMany(Project::class);
+    }
+
+    public function tasks(): HasManyThrough|User
+    {
+        return $this->hasManyThrough(Task::class, Project::class);
     }
 }
