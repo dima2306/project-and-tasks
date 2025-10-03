9012d6790bd19853db99ad7a40b0ac8def230164
dima23 <dima.simonishvili@gmail.com>
2025-10-03 13:51:08 +0400

    Fixes TaskFactory with status management methods and integrates TaskStatus enum
    Signed-off-by: dima23 <dima.simonishvili@gmail.com>

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -9,6 +9,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\TaskStatus;
 use App\Models\Project;
 use App\Models\Task;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -20,13 +21,34 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => $this->faker->title(),
-            'description' => $this->faker->text(),
-            'status' => $this->faker->word(),
-            'created_at' => now(),
-            'updated_at' => now(),
-
+            'title' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(),
+            'status' => $this->faker->randomElement(TaskStatus::values()),
             'project_id' => Project::factory(),
         ];
+    }
+
+    public function todo(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'status' => TaskStatus::TODO,
+            'completed_at' => null,
+        ]);
+    }
+
+    public function inProgress(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'status' => TaskStatus::IN_PROGRESS,
+            'completed_at' => null,
+        ]);
+    }
+
+    public function completed(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'status' => TaskStatus::COMPLETED,
+            'completed_at' => $this->faker->dateTimeBetween('-1 month', 'now'),
+        ]);
     }
 }
